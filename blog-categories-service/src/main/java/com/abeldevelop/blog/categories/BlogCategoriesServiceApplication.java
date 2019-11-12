a6fb8e977d2985f5e8f25227fd02d199d041bd32
Abel Fernandez <abelfergodevelop@gmail.com>
2019-11-12 21:41:22 +0100

    Create basic service and test for swagger-codegen-maven-plugin

---
+++ blog-categories-service/src/main/java/com/abeldevelop/blog/categories/BlogCategoriesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.abeldevelop.blog.categories;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogCategoriesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogCategoriesServiceApplication.class, args);
+	}
+
+}
---
+++ blog-categories-service/src/test/java/com/abeldevelop/blog/categories/BlogCategoriesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abeldevelop.blog.categories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogCategoriesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
