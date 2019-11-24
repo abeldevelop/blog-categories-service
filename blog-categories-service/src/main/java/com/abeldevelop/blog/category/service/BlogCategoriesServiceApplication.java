498bee147c401c82f3a3dc73fb5bbd0dd56c8198
Abel Fernandez <abelfergodevelop@gmail.com>
2019-11-24 17:11:43 +0100

    Refactor properties

--- blog-categories-service/src/main/java/com/abeldevelop/blog/category/service/BlogCategoriesServiceApplication.java
+++ blog-categories-service/src/main/java/com/abeldevelop/blog/category/service/BlogCategoriesServiceApplication.java
@@ -3,12 +3,14 @@ package com.abeldevelop.blog.category.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @ComponentScan({"${abeldevelop.configuration.component-scan}"})
 @EntityScan("${abeldevelop.configuration.entity-scan}")
 @EnableJpaRepositories("${abeldevelop.configuration.enable-jpa-repositories}")
+@EnableDiscoveryClient
 @SpringBootApplication
 public class BlogCategoriesServiceApplication {
 
