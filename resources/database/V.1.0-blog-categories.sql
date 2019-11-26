2f9f95a88dc408e4230ee85e50181d4b30a2dfbc
Abel Fernandez <abelfergodevelop@gmail.com>
2019-11-26 20:00:53 +0100

    Add state field in database to control transactionality. Does not compile, pending modify CategoryEntity

---
+++ resources/database/V.1.0-blog-categories.sql
@@ -0,0 +1,9 @@
+CREATE SCHEMA IF NOT EXISTS blog_db;
+
+CREATE TABLE blog_db.categories (
+  code varchar(25) NOT NULL,
+  name varchar(25) NOT NULL,
+  state varchar(25) NOT NULL DEFAULT 'ENABLED',
+  PRIMARY KEY (code),
+  UNIQUE KEY `BLOG_DB_CATEGORIES_UNIQUE_NAME` (`name`)
+);
