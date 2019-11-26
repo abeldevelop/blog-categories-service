6a76004e4c7f4fba1de8d2bd3175a261ade8b208
Abel Fernandez <abelfergodevelop@gmail.com>
2019-11-26 20:06:37 +0100

    Update sql

--- resources/database/V.1.0-blog-categories.sql
+++ resources/database/V.1.0-blog-categories.sql
@@ -3,7 +3,7 @@ CREATE SCHEMA IF NOT EXISTS blog_db;
 CREATE TABLE blog_db.categories (
   code varchar(25) NOT NULL,
   name varchar(25) NOT NULL,
-  state varchar(25) NOT NULL DEFAULT 'ENABLED',
+  state varchar(25) NOT NULL,
   PRIMARY KEY (code),
   UNIQUE KEY `BLOG_DB_CATEGORIES_UNIQUE_NAME` (`name`)
 );
